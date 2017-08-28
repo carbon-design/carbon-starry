a62793c7848e767d348612d053e4ee477f2404c7
Aford <244734834@qq.com>
2017-08-28 17:00:14 +0800

    fix details

--- src/utils/requester.js
+++ src/utils/requester.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default {
   install (Vue, options) {
-    Vue.prototype.fetch = axios
+    // Vue.prototype.fetch = axios
+    Object.defineProperty(Vue.prototype, 'fetch', { value: axios })
   }
 }

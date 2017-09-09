297479fa55f6f609044e183357d81809cfff1c3e
Aford <244734834@qq.com>
2017-09-09 12:35:04 +0800

    modified login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,45 @@
 <template>
-  <article class="app-login">
-    login
+  <article class="app-login" ref="loginPage">
+    <div class="app-avatar"><img src="~/#/images/avatar.jpg" alt="avatar"></div>
+    <div class="form">
+      <div class="fm-item">
+        <div class="label">用户账号</div>
+        <div class="ipt-wrap">
+          <input type="text" placeholder="请输入账户名/手机号">
+        </div>
+      </div>
+      <div class="fm-item">
+        <div class="label">密码</div>
+        <div class="ipt-wrap">
+          <input type="password" placeholder="请输入账户密码">
+        </div>
+      </div>
+    </div>
+    <div class="foot">
+      <button class="loginBtn">登  录</button>
+      <div class="logo"></div>
+    </div>
   </article>
 </template>
 
+<style lang="scss" src="#/styles/pages/login.scss"></style>
+
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      userName: '',
+      password: ''
+    }
+  },
+  mounted () {
+    const { loginPage } = this.$refs
+    const { clientWidth, clientHeight } = document.documentElement
+    loginPage.style.width = `${clientWidth}px`
+    loginPage.style.minHeight = `${clientHeight}px`
+    loginPage.style.paddingBottom = `${clientHeight * 0.42}px`
+    loginPage.style.paddingTop = `${clientHeight * 0.06}px`
+  }
 }
 </script>

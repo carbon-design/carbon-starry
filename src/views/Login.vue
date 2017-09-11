ebb9824c4126ed4f6928a7ba271e60620d0f6a81
Aford <244734834@qq.com>
2017-09-11 21:37:41 +0800

    add alpaca icon

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,13 +5,13 @@
       <div class="fm-item">
         <div class="label">用户账号</div>
         <div class="ipt-wrap">
-          <input type="text" placeholder="请输入账户名/手机号" v-model="userName">
+          <input type="text" placeholder="请输入账户名/手机号" v-model="userName" @focus="userFocus" @blur="userBlur">
         </div>
       </div>
       <div class="fm-item">
         <div class="label">密码</div>
         <div class="ipt-wrap">
-          <input type="password" placeholder="请输入账户密码" v-model="password">
+          <input type="password" placeholder="请输入账户密码" v-model="password" @focus="passwordFocus" @blur="passwordBlur">
         </div>
       </div>
     </div>
@@ -19,6 +19,7 @@
       <button class="loginBtn" @click="submit">登  录</button>
       <div class="logo"></div>
     </div>
+    <div class="alpaca" :class="{ showAll: idFocus, showLittle: pswdFocus }"></div>
   </article>
 </template>
 
@@ -34,24 +35,40 @@ export default {
   data () {
     return {
       userName: '',
-      password: ''
+      password: '',
+      idFocus: false,
+      pswdFocus: false
     }
   },
   mounted () {
     const { loginPage } = this.$refs
-    const { clientWidth, clientHeight } = document.documentElement
-    loginPage.style.cssText = `width: ${clientWidth}px; min-height: ${clientHeight}px; padding-botttom: ${clientHeight * 0.42}px; padding-top: ${clientHeight * 0.06}px`
+    const { clientHeight } = document.documentElement
+    loginPage.style.cssText = `min-height: ${clientHeight}px; padding: ${clientHeight * 0.06}px 0 ${clientHeight * 0.42}px 0;`
   },
   methods: {
     ...mapActions({
       login: 'login'
     }),
+    userFocus () {
+      this.idFocus = true
+      this.pswdFocus = false
+    },
+    userBlur () {
+      this.idFocus = false
+    },
+    passwordFocus () {
+      this.idFocus = false
+      this.pswdFocus = true
+    },
+    passwordBlur () {
+      this.pswdFocus = false
+    },
     async submit () {
-      const { indicator, userName, password, $router, login } = this
+      const { indicator, toast, userName, password, $router, login } = this
       if (!userName.trim()) {
-        alert('请输入用户名！')
+        toast('请输入用户名！', 'bottom', 2000)
       } else if (!password.trim()) {
-        alert('请输入密码！')
+        toast('请输入密码！', 'bottom', 2000)
       } else {
         indicator.open('正在登录...')
         const resLogin = await getLogin({

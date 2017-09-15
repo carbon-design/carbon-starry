fe124cf382e8c8aacb926f773b7e8b3f85abb0c3
Aford <244734834@qq.com>
2017-09-15 16:25:22 +0800

    modified channel page

--- src/views/Channel/Channel.vue
+++ src/views/Channel/Channel.vue
@@ -1,25 +1,70 @@
 <template>
   <article class="page-channel">
+    <div class="page-header">
+      <i class="iconfont left" v-if="editState" @click="cancelModify">&#xe629;</i>
+      <span>金融课程</span>
+      <i class="iconfont" v-if="!editState" @click="doModify">&#xe6eb;</i>
+      <i class="iconfont" v-if="editState">&#xe64b;</i>
+    </div>
     <div class="list-group">
       <div class="list-title">
+        <div class="app-checkbox checked"></div>
         <div class="title">
           <h1>金融理财基本小常识</h1>
           <p>主讲人：柯银明</p>
         </div>
-        <div class="time">2017/09/16 12:00</div>
+        <div class="time">09/16 12:00</div>
       </div>
       <div class="lesson-list">
         <div class="lesson-item">
+          <div class="app-checkbox checked"></div>
           <div class="info">
             <h1>如何正确理解投资收益和回报</h1>
             <p>时长：50分钟</p>
           </div>
           <div class="mode">难度：中等</div>
         </div>
+      </div>
+    </div>
+    <div class="list-group">
+      <div class="list-title">
+        <div class="app-checkbox checked"></div>
+        <div class="title">
+          <h1>金融理财中级进阶知识</h1>
+          <p>主讲人：柯银明</p>
+        </div>
+        <div class="time">09/30 12:00</div>
+      </div>
+      <div class="lesson-list">
         <div class="lesson-item">
+          <div class="app-checkbox checked"></div>
           <div class="info">
-            <h1>如何建立有效的投资反馈机制</h1>
-            <p>时长：40分钟</p>
+            <h1>深入剖析资本流动与市场经济的关系</h1>
+            <p>时长：30分钟</p>
+          </div>
+          <div class="mode">难度：较难</div>
+        </div>
+        <div class="lesson-item">
+          <div class="app-checkbox checked"></div>
+          <div class="info">
+            <h1>浅析中国金融理财及货币基金的投资原理</h1>
+            <p>时长：20分钟</p>
+          </div>
+          <div class="mode">难度：较难</div>
+        </div>
+        <div class="lesson-item">
+          <div class="app-checkbox checked"></div>
+          <div class="info">
+            <h1>为实现稳定的投资收益模式打造投资模型</h1>
+            <p>时长：20分钟</p>
+          </div>
+          <div class="mode">难度：较难</div>
+        </div>
+        <div class="lesson-item">
+          <div class="app-checkbox checked"></div>
+          <div class="info">
+            <h1>如何面对投资风险及风险控制手段分析</h1>
+            <p>时长：20分钟</p>
           </div>
           <div class="mode">难度：较难</div>
         </div>
@@ -34,9 +79,14 @@ export default {
   name: 'channel',
   data () {
     return {
+      editState: false
     }
   },
   components: {
+  },
+  beforeCreate () {
+    this.bodyClass = document.documentElement.classList
+    this.bodyClass.add('bg-light-white')
   },
   beforeMount () {
   },
@@ -45,8 +95,15 @@ export default {
   mounted () {
   },
   beforeDestroy () {
+    this.bodyClass.remove('bg-light-white')
   },
   methods: {
+    doModify () {
+      this.editState = true
+    },
+    cancelModify () {
+      this.editState = false
+    }
   }
 }
 </script>

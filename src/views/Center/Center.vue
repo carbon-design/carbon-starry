5a3070d84b83c8973febf7e8ae45b697007fd380
Aford <244734834@qq.com>
2017-09-21 21:35:46 +0800

    modified center

--- src/views/Center/Center.vue
+++ src/views/Center/Center.vue
@@ -11,7 +11,17 @@
           <h2>Annie Joe</h2>
         </div>
       </div>
-      <div class="status"></div>
+      <div class="status">
+        <div class="chart">
+          <h1>积分走势</h1>
+          <div class="line" ref="line"></div>
+        </div>
+        <div class="progress">
+          <h1>升级进度</h1>
+          <div class="percent"><b>{{ percent }}</b> %</div>
+          <div class="bar"><i :style="{ width: percent + '%' }"></i></div>
+        </div>
+      </div>
     </div>
     <app-loader :isFixed="false"></app-loader>
   </article>
@@ -19,11 +29,14 @@
 
 <script>
 import AppLoader from '^/DotLoader'
+import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/line'
 
 export default {
   name: 'center',
   data () {
     return {
+      percent: 68
     }
   },
   components: {
@@ -35,11 +48,72 @@ export default {
     this.bodyClass.add('app-theme-dark')
   },
   beforeMount () {
+
   },
   computed: {
   },
   mounted () {
-    this.$refs.main.style.minHeight = this.docEl.clientHeight - 0.94 * window.rootFontSize + 'px'
+    const { $refs: { main, line }, docEl } = this
+    main.style.minHeight = docEl.clientHeight - 0.94 * window.rootFontSize + 'px'
+    line.style.cssText = `width: ${line.offsetWidth}px; height: ${line.offsetHeight}`
+    echarts.init(line).setOption({
+      grid: {
+        show: true,
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+        borderWidth: 0,
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      },
+      xAxis: [{
+        axisLine: {
+          onZero: false,
+          show: false
+        },
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          show: false
+        },
+        type: 'category',
+        boundaryGap: false,
+        data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+      }],
+      yAxis: [{
+        scale: true,
+        type: 'value',
+        axisLine: {
+          show: false
+        },
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          show: false
+        }
+      }],
+      series: [{
+        smooth: true,
+        name: '贷款',
+        type: 'line',
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            width: 3 * window.dpr,
+            color: '#6adaff'
+          }
+        },
+        data: [0, 1, 3, 2, 2, 3, 2, 4, 5, 8, 9, 12]
+      }]
+    })
   },
   beforeDestroy () {
     this.bodyClass.remove('app-theme-dark')
